1a52479eedf4013a507ca123153f8fe526c0fbb4
Kenneth <kenneth.james.carroll@gmail.com>
2021-03-01 14:23:10 -0500

    Beginning work on updating shcema

--- ManalyticsBackend/Models/Card.cs
+++ ManalyticsBackend/Models/Card.cs
@@ -4,5 +4,6 @@ namespace ManalyticsBackend.Models
     {
         public int Id {get; set;}
         public string Title {get; set;}
+        public List<PrimaryType> PrimaryTypes {get; set;}
     }
 }
